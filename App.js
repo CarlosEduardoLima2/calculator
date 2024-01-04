8e2da8d799b42db81607868474022b513b4cee1f
Carlos Eduardo <carloseduardolimabastosm@gmail.com>
2024-01-04 02:02:01 -0300

    update button styles

--- App.js
+++ App.js
@@ -6,27 +6,40 @@ import Display from './src/components/Display';
 
 export default function App() {
   const [displayValue, setDisplayValue] = useState(0)
+
+  const addDigital = (n) => {
+    setDisplayValue(n)
+  }
+
+  const clearMemory = () => {
+    setDisplayValue('0')
+  }
+
+  const setOparation = (oparation) => {
+
+  }
+
   return (
     <View style={styles.container}>
       <Display value={displayValue}/>
       <View style={styles.buttons}>
-        <Button label="AC"/>
-        <Button label="/"/>
-        <Button label="7"/>
-        <Button label="8"/>
-        <Button label="9"/>
-        <Button label="*"/>
-        <Button label="4"/>
-        <Button label="5"/>
-        <Button label="6"/>
-        <Button label="-"/>
-        <Button label="1"/>
-        <Button label="2"/>
-        <Button label="3"/>
-        <Button label="+"/>
-        <Button label="0"/>
-        <Button label="."/>
-        <Button label="="/>
+        <Button label="AC" triple onClick={clearMemory}/>
+        <Button label="/" operation onClick={setOparation}/>
+        <Button label="7" onClick={addDigital}/>
+        <Button label="8" onClick={addDigital}/>
+        <Button label="9" onClick={addDigital}/>
+        <Button label="*" operation onClick={setOparation}/>
+        <Button label="4" onClick={addDigital}/>
+        <Button label="5" onClick={addDigital}/>
+        <Button label="6" onClick={addDigital}/>
+        <Button label="-" operation onClick={setOparation}/>
+        <Button label="1" onClick={addDigital}/>
+        <Button label="2" onClick={addDigital}/>
+        <Button label="3" onClick={addDigital}/>
+        <Button label="+" operation onClick={setOparation}/>
+        <Button label="0" double onClick={addDigital}/>
+        <Button label="." onClick={addDigital}/>
+        <Button label="=" operation onClick={setOparation}/>
       </View>
     </View>
   );
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,17 @@
 import { Text, Dimensions, TouchableHighlight } from 'react-native';
 import { styles } from './styles';
 
-export default function Button({onClick, label}) {
+export default function Button({onClick, label, double, triple, operation}) {
+
+  const stylesButton = [styles.button];
+
+  if(double) stylesButton.push(styles.buttonDouble)
+  if(triple) stylesButton.push(styles.buttonTriple)
+  if(operation) stylesButton.push(styles.oparationButton)
+
  return (
-   <TouchableHighlight onPress={onClick}>
-    <Text style={styles.button}>{label}</Text>
+   <TouchableHighlight onPress={() => onClick(label)}>
+    <Text style={stylesButton}>{label}</Text>
    </TouchableHighlight>
   );
 }
